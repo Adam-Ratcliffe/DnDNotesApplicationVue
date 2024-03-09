3d929b40ccb42ba4a86b0a77f8c69d5eb4c901f3
Adam-Ratcliffe <59871496+Adam-Ratcliffe@users.noreply.github.com>
2024-03-09 19:20:33 +0000

    Added password protection
    Added password text file to the gitignore file so it's hidden from the repo

--- DnDNotesAppVue.Server/Services/MongoDBService.cs
+++ DnDNotesAppVue.Server/Services/MongoDBService.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MongoDB.Bson;
 using DnDNotesAppVue.Server.Models;
+using System.IO;
 
 namespace DnDNotesAppVue.Services;
 
@@ -9,14 +10,21 @@ public class MongoDBService
 {
 
     private readonly IMongoCollection<Note> _noteCollection;
+    private readonly IMongoCollection<Character> _characterCollection;
+    private const string _passwordFileName = "password.txt";
 
     public MongoDBService(IOptions<NotesDatabase> mongoDBSettings)
     {
-        MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+        StreamReader sr = new StreamReader(_passwordFileName);
+
+        MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI.Replace("#", sr.ReadLine()));
         IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
         _noteCollection = database.GetCollection<Note>(mongoDBSettings.Value.CollectionName);
-    }
+        _characterCollection = database.GetCollection<Character>(mongoDBSettings.Value.CharacterCollection);
 
+        sr.Close();
+    }
+    #region Generic Notes Methods
     public async Task<List<Note>> GetAsync()
     {
         return await _noteCollection.Find(new BsonDocument()).ToListAsync();
@@ -31,5 +39,36 @@ public class MongoDBService
         FilterDefinition<Note> filter = Builders<Note>.Filter.Eq("Id", id);
         await _noteCollection.DeleteOneAsync(filter);
     }
+    #endregion
 
+    #region Character Notes Methods
+    public async Task<List<Character>> GetAllCharactersAsync()
+    {
+        return await _characterCollection.Find(new BsonDocument()).ToListAsync();
+    }
+
+    public async Task<Character> GetCharacter(string id)
+    {
+        FilterDefinition<Character> filter = Builders<Character>.Filter.Eq("Id", id);
+        return await _characterCollection.Find(filter).FirstAsync();
+    }
+
+    public async Task UpdateCharacter(Character character)
+    {
+        FilterDefinition<Character> filter = Builders<Character>.Filter.Eq("Id", character.Id);
+        await _characterCollection.ReplaceOneAsync(filter, character);
+    }
+
+    public async Task CreateCharacterAsync(Character character)
+    {
+        await _characterCollection.InsertOneAsync(character);
+        return;
+    }
+
+    public async Task DeleteCharacterAsync(string id)
+    {
+        FilterDefinition<Character> filter = Builders<Character>.Filter.Eq("Id", id);
+        await _characterCollection.DeleteOneAsync(filter);
+    }
+    #endregion
 }
